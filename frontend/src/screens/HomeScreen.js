7ee48e1968b116bc820f16dcc78d43f7e0891e60
Caner Kuncu <warlordsw@gmail.com>
2021-07-04 04:25:04 +0300

    add dispatch and reducers hooks to homescreen.js and productscreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import data from '../data'
+import React, { useEffect } from 'react'
 import Product from '../components/Product'
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
+import { useDispatch, useProductState } from '../context/context'
+import { listProducts } from '../context/action'
 
 function HomeScreen() {
+  const dispatch = useDispatch()
+  const { products, loading, error } = useProductState()
+
+  useEffect(() => {
+    const listProductsFunc = async () => {
+      await listProducts(dispatch)
+    }
+    listProductsFunc()
+  }, [dispatch])
+
   return (
     <div>
-      <div className='row center'>
-        {data.products.map((product) => {
-          return <Product key={product._id} product={product} />
-        })}
-      </div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <div className='row center'>
+          {products.map((product) => {
+            return <Product key={product._id} product={product} />
+          })}
+        </div>
+      )}
     </div>
   )
 }
