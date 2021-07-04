890fadea819118b8c6e5cab8cbdb8242da65f4d5
Caner Kuncu <warlordsw@gmail.com>
2021-07-04 04:20:46 +0300

    add StoreProvider to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,32 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import { StoreProvider } from './context/context'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='grid-container'>
-        <header className='row'>
-          <div>
-            <a className='brand' href='/'>
-              amazonya
-            </a>
-          </div>
-          <div>
-            <a href='/cart'>Cart</a>
-            <a href='/signin'>Sign in</a>
-          </div>
-        </header>
-        <main>
-          <Route path='/product/:id' component={ProductScreen}></Route>
-          <Route path='/' component={HomeScreen} exact></Route>
-        </main>
-        <footer className='row center'>All right reserved</footer>
-      </div>
-    </BrowserRouter>
+    <StoreProvider>
+      <BrowserRouter>
+        <div className='grid-container'>
+          <header className='row'>
+            <div>
+              <a className='brand' href='/'>
+                amazdokuz
+              </a>
+            </div>
+            <div>
+              <a href='/cart'>Cart</a>
+              <a href='/signin'>Sign in</a>
+            </div>
+          </header>
+          <main>
+            <Route path='/product/:id' component={ProductScreen}></Route>
+            <Route path='/' component={HomeScreen} exact></Route>
+          </main>
+          <footer className='row center'>All right reserved</footer>
+        </div>
+      </BrowserRouter>
+    </StoreProvider>
   )
 }
 
