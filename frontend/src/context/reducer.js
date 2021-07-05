848148791e5e023dbd7fcf5e6701b18480ebcbaa
Caner Kuncu <warlordsw@gmail.com>
2021-07-05 13:38:18 +0300

    add CART_ADD_ITEM an CART_REMOVE_ITEM to cartConstants.js and reducer.js

--- frontend/src/context/reducer.js
+++ frontend/src/context/reducer.js
@@ -1,3 +1,4 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
@@ -10,6 +11,9 @@ import {
 export const initialState = {
   products: [],
   product: {},
+  cartItems: localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [],
   loading: true,
   error: null,
 }
@@ -28,6 +32,29 @@ export const productListReducer = (initialState, action) => {
       return { ...initialState, loading: false, product: action.payload }
     case PRODUCT_DETAILS_FAIL:
       return { ...initialState, loading: false, error: action.payload }
+    case CART_ADD_ITEM:
+      const item = action.payload
+      const existItem = initialState.cartItems.find(
+        (x) => x.product === item.product
+      )
+      if (existItem) {
+        return {
+          ...initialState,
+          cartItems: initialState.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        }
+      } else {
+        return { ...initialState, cartItems: [...initialState.cartItems, item] }
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...initialState,
+        cartItems: initialState.cartItems.filter(
+          (x) => x.product !== action.payload
+        ),
+      }
+
     default:
       return { ...initialState }
   }
