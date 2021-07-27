7446d1c4a4b5b96828c34cd0604868b95c05b15c
Caner Kuncu <warlordsw@gmail.com>
2021-07-27 03:33:00 +0300

    add database connect and express.json() to recognize object as json

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,32 @@
 import express from 'express'
-import data from './data.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import productRouter from './routers/productRouter.js'
+import userRouter from './routers/userRouter.js'
 
+dotenv.config()
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((product) => product._id === req.params.id)
-  if (product) {
-    res.send(product)
-  } else {
-    res.status(404).send({ message: 'Product not Found' })
-  }
+const CONNECTION_URL =
+  'mongodb+srv://caner:caCA0726@cluster0.x3bft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 })
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products)
-})
+app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
 
 app.get('/', (req, res) => {
   res.send('Server is ready')
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message })
 })
 
 const port = process.env.PORT || 5000
