37cabe414141c4e60d70edc126239b4909622544
Caner Kuncu <warlordsw@gmail.com>
2021-08-03 01:55:55 +0300

    add order reducers and actions

--- frontend/src/context/action.js
+++ frontend/src/context/action.js
@@ -1,9 +1,22 @@
 import Axios from 'axios'
 import {
   CART_ADD_ITEM,
+  CART_EMPTY,
   CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants'
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+} from '../constants/orderConstants'
 import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
@@ -74,6 +87,10 @@ export const saveShippingAddress = (dispatch, data) => {
   localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
 
+export const savePaymentMethod = (dispatch, data) => {
+  dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data })
+}
+
 export const signin = async (dispatch, email, password) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } })
   try {
@@ -113,8 +130,66 @@ export const register = async (dispatch, name, email, password) => {
   }
 }
 
+export const createOrder = async (dispatch, order, token) => {
+  dispatch({ type: ORDER_CREATE_REQUEST, payload: order })
+  try {
+    const { data } = await Axios.post('/api/orders', order, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data.order })
+    dispatch({ type: CART_EMPTY })
+    localStorage.removeItem('cartItems')
+    localStorage.setItem('orderItems', JSON.stringify(data.order))
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const detailsOrder = async (dispatch, orderId, token) => {
+  dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId })
+  try {
+    const { data } = await Axios.get(`/api/orders/${orderId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({ type: ORDER_DETAILS_FAIL, payload: message })
+  }
+}
+
+export const payOrder = async (dispatch, order, token) => {
+  dispatch({ type: ORDER_PAY_REQUEST, payload: order })
+  try {
+    const { data } = await Axios.put(`/api/orders/${order._id}/pay`, order, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    dispatch({ type: ORDER_PAY_SUCCESS, payload: data })
+    localStorage.setItem('orderItems', JSON.stringify(data.order))
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    dispatch({ type: ORDER_PAY_FAIL, payload: message })
+  }
+}
+
 export const signout = (dispatch) => {
   localStorage.removeItem('userInfo')
   localStorage.removeItem('cartItems')
+  localStorage.removeItem('shippingAddress')
   dispatch({ type: USER_SIGNOUT })
 }
--- frontend/src/context/reducer.js
+++ frontend/src/context/reducer.js
@@ -1,8 +1,23 @@
 import {
   CART_ADD_ITEM,
+  CART_EMPTY,
   CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants'
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_RESET,
+  ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_RESET,
+  ORDER_PAY_SUCCESS,
+} from '../constants/orderConstants'
 import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
@@ -29,20 +44,30 @@ export const initialState = {
     : [],
   shippingAddress: localStorage.getItem('shippingAddress')
     ? JSON.parse(localStorage.getItem('shippingAddress'))
-    : {},
+    : { fullName: '', address: '', city: '', postalCode: '', country: '' },
   loading: false,
   error: null,
   userInfo: localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
+  paymentMethod: 'Paypal',
+  order: localStorage.getItem('orderItems')
+    ? JSON.parse(localStorage.getItem('orderItems'))
+    : {},
+  success: false,
 }
 export const productListReducer = (initialState, action) => {
   //console.log(initialState, action)
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { ...initialState, loading: true }
+      return { ...initialState, loading: true, error: null }
     case PRODUCT_LIST_SUCCESS:
-      return { ...initialState, loading: false, products: action.payload }
+      return {
+        ...initialState,
+        loading: false,
+        products: action.payload,
+        error: null,
+      }
     case PRODUCT_LIST_FAIL:
       return { ...initialState, loading: false, error: action.payload }
     case PRODUCT_DETAILS_REQUEST:
@@ -121,8 +146,89 @@ export const productListReducer = (initialState, action) => {
         loading: false,
         error: action.payload,
       }
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...initialState,
+        paymentMethod: action.payload,
+      }
+    case CART_EMPTY:
+      return {
+        ...initialState,
+        cartItems: [],
+      }
+    case ORDER_CREATE_REQUEST:
+      return {
+        ...initialState,
+        loading: true,
+        error: null,
+      }
+    case ORDER_CREATE_SUCCESS:
+      return {
+        ...initialState,
+        loading: false,
+        success: true,
+        order: action.payload,
+        error: null,
+      }
+    case ORDER_CREATE_FAIL:
+      return {
+        ...initialState,
+        loading: false,
+        error: action.payload,
+      }
+    case ORDER_CREATE_RESET:
+      return {
+        ...initialState,
+        error: null,
+        loading: false,
+      }
+    case ORDER_DETAILS_REQUEST:
+      return {
+        ...initialState,
+        loading: true,
+        error: null,
+      }
+    case ORDER_DETAILS_SUCCESS:
+      return {
+        ...initialState,
+        loading: false,
+        order: action.payload,
+        error: null,
+      }
+    case ORDER_DETAILS_FAIL:
+      return {
+        ...initialState,
+        loading: false,
+        error: action.payload,
+      }
+    case ORDER_PAY_REQUEST:
+      return {
+        ...initialState,
+        loading: true,
+        error: null,
+      }
+    case ORDER_PAY_SUCCESS:
+      return {
+        ...initialState,
+        loading: false,
+        success: true,
+        error: null,
+      }
+    case ORDER_PAY_FAIL:
+      return {
+        ...initialState,
+        loading: false,
+        error: action.payload,
+      }
+    case ORDER_PAY_RESET:
+      return {
+        ...initialState,
+        loading: false,
+        error: null,
+        success: false,
+      }
 
     default:
-      return { ...initialState }
+      return initialState
   }
 }
