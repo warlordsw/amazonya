58511542f28bd414ec5593d5375bec57a8a2b1b4
Caner Kuncu <warlordsw@gmail.com>
2021-08-03 01:52:27 +0300

    add middleware to check user token

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,25 @@ export const generateToken = (user) => {
     }
   )
 }
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization
+  console.log(authorization)
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length)
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'somethingsecret',
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' })
+        } else {
+          req.user = decode
+          next()
+        }
+      }
+    )
+  } else {
+    res.status(401).send({ message: 'No Token' })
+  }
+}
